9568f32da6ef8c05cc62bd6f613078bd74dce1a1
Richard <51269862+MeRichard123@users.noreply.github.com>
2020-07-24 17:06:05 +0100

    Tailwind reconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       softRed: "hsl(0, 93%, 68%)",
       red: "hsl(0, 36%, 70%)",
     },
+    letterSpacing: {
+      widest: "0.5em",
+    },
   },
   variants: {},
   plugins: [],
